9253a3aac153fe67f14710375f6f89be794c5aaa
Samuel Barbosa Almeida <barbosasamuel24@gmail.com>
2021-08-30 20:27:00 -0300

    Gender filter added

--- src/app/core/filmes.service.ts
+++ src/app/core/filmes.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
+import { ConfigPrams } from '../shared/models/config-prams';
+import { ConfigParamsService } from './config-params.service';
 
-const url = 'http://localhost:4200/filmes/';
+const url = 'http://localhost:3000/filmes/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmesService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private configService: ConfigParamsService) { }
 
   salvar(filme: Filme): Observable<Filme> {
     return this.http.post<Filme>(url, filme);
+  }
+
+  listar(config: ConfigPrams): Observable<Filme[]> {
+    const configPrams = this.configService.configurarParametros(config);
+    return this.http.get<Filme[]>(url, {params: configPrams});
   }
 }
--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { FilmesService } from 'src/app/core/filmes.service';
+import { Filme } from 'src/app/shared/models/filme';
+import { ConfigPrams } from 'src/app/shared/models/config-prams';
 
 @Component({
   selector: 'dio-listagem-filmes',
@@ -7,13 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListagemFilmesComponent implements OnInit {
 
-  constructor() { }
+  config: ConfigPrams = {
+    pagina: 0,
+    limite: 4
+  };
+  filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
-  ngOnInit() {
+  constructor(private filmesService: FilmesService,
+              private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+      this.config.pesquisa = val;
+      this.resetarConsulta();
+    });
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.config.campo = {tipo: 'genero', valor: val};
+      this.resetarConsulta();
+    });
+
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção cientifica', 'Comédia', 'Aventura', 'Drama'];
+
+    this.listarFilmes();
   }
 
-  open() {
+  onScroll(): void {
+    this.listarFilmes();
   }
 
+  private listarFilmes(): void {
+    this.config.pagina++;
+    this.filmesService.listar(this.config)
+      .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.config.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
+  }
 }
